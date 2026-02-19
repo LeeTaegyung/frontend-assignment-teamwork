18aa04929fa03854c784941091c3d7ed799c3809
taegyung <taegyung.e@gmail.com>
2026-02-19 16:52:24 +0900

    style: 메뉴열기 위치 변경 및 도면 메뉴 active 처리

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,14 +16,14 @@ export default function Header() {
     .map((key: string) => drawingMap[key]);
 
   return (
-    <header className='shrink-0 grow-0'>
+    <header className='relative shrink-0 grow-0'>
       <h1 className='border-b-border border-b px-3 py-2 text-lg font-bold'>
         <Link to='/'>
           {project.name} - {drawingMap[id ?? '00'].name}
         </Link>
       </h1>
       <button
-        className='fixed bottom-5 left-5'
+        className='absolute top-1/2 right-3 -translate-y-1/2'
         onClick={() => {
           setOpen((o) => !o);
         }}
@@ -33,7 +33,7 @@ export default function Header() {
       {open &&
         createPortal(
           <div className='fixed top-0 right-0 bottom-0 left-0 bg-black/50'>
-            <div className='relative h-full w-[30%] overflow-auto bg-white'>
+            <div className='relative ml-auto h-full w-[30%] overflow-auto bg-white'>
               <button
                 onClick={() => {
                   setOpen((o) => !o);
@@ -45,7 +45,10 @@ export default function Header() {
               <ul className='p-10'>
                 {drawings.map((data) => (
                   <li key={data.id} className='border-b-border border-b'>
-                    <Link to={`/drawing/${data.id}`} className='block py-5'>
+                    <Link
+                      to={`/drawing/${data.id}`}
+                      className={`block py-5 ${id === data.id && 'text-primary'}`}
+                    >
                       {data.name}
                     </Link>
                   </li>
